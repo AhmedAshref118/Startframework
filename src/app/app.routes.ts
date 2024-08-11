7126515c37b68c7155cfad4547ad2bfdb5ed0584
ahmed ashref ahmed <amed.mody202120@gmail.com>
2024-08-11 17:13:31 +0300

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'START FREMWORK',component:HomeComponent},
+    {path:'ABOUT',component:AboutComponent},
+    {path:'PORTFOLIO',component:PortfolioComponent},
+    {path:'CONTACT',component:ContactComponent},
+    {path:'**',component:NotfoundComponent},
+    
+];
